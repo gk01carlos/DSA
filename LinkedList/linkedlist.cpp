c13bd58dc8c78cff6497e36fa6344c86c9877c8f
gk01carlos <gk01carlos@gmail.com>
2024-09-22 09:50:25 -0600

    linked list - node data type created

---
+++ LinkedList/linkedlist.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// To create a linked list first we have to create the node data type.
+class Node {
+    int value;
+    Node *next;
+
+    Node(int value) {
+        this->value = value;
+        this->next = nullptr;
+    } 
+};
+
+int main() {
+    
+    return 0;
+}
