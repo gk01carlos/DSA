38741913929b8924b9c09c6f00141137d3b52a3e
gk01carlos <gk01carlos@gmail.com>
2024-09-22 11:40:49 -0600

    linked list - comments deleted

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// To create a linked list first we have to create the node data type.
 class Node {
     public:
         int value;
@@ -22,13 +21,10 @@ class LinkedList {
         }
 
         void insertFront(int value) {
-            // 1. we have to create a new node.
             Node *newNode = new Node(value);
 
-            // 2. we have to set the next of the new node to the current head
             newNode->next = head;
 
-            // 3. we have set the head as the new node.
             head = newNode;
         }
 
