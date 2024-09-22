55a68d13a2da330ffab032c95ab8eebb893cecb9
gk01carlos <gk01carlos@gmail.com>
2024-09-22 09:56:41 -0600

    linked list - function to insert an element at the front of the list

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -3,16 +3,37 @@ using namespace std;
 
 // To create a linked list first we have to create the node data type.
 class Node {
-    int value;
-    Node *next;
+    public:
+        int value;
+        Node *next;
 
-    Node(int value) {
-        this->value = value;
-        this->next = nullptr;
-    } 
+        Node(int value) {
+            this->value = value;
+            this->next = nullptr;
+        } 
+};
+
+class LinkedList {
+    private: 
+        Node *head;
+    public:
+        LinkedList() {
+            this->head = nullptr;
+        }
+
+        void insertFront(int value) {
+            // 1. we have to create a new node.
+            Node *newNode = new Node(value);
+
+            // 2. we have to set the next of the new node to the current head
+            newNode->next = head;
+
+            // 3. we have set the head as the new node.
+            head = newNode;
+        }
 };
 
 int main() {
-    
+
     return 0;
 }
